63007211b3ff90276ddb31e943e675e48ac21321
Barry Cooper <neftleonhudozhnik@gmail.com>
2025-02-22 13:26:08 +0500

    Добавлен урок 3.2 Шаг 6

---
+++ selenium_course/lesson3_2_step6.py
@@ -0,0 +1,16 @@
+actual_result = "abrakadabra"
+print("Wrong text, got " + actual_result + ", something wrong")
+
+##"Let's count together: {}, then goes {}, and then {}".format("one", "two", "three")
+
+print("Let's count together: {}, then goes {}, and then {}".format("one", "two", "three"))
+
+str1 = "one"
+str2 = "two"
+str3 = "three"
+print(f"Let's count together: {str1}, then goes {str2}, and then {str3}")
+
+actual_result = "abrakadabra"
+print(f"Wrong text, got {actual_result}, something wrong")
+
+print(f"{2+3}")
